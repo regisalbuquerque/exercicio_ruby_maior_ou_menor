cf09443e4974acf669949921d3f958c8c9a12e32
Regis Albuquerque <regis.albuquerque1@gmail.com>
2020-04-26 19:34:15 -0300

    Codificação aula 3

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,31 +1,49 @@
-puts "Bem-vindo ao jogo da adivinhação"
-puts "Qual é o seu nome?"
-nome = gets
-puts "\n\n\n\n\n\n"
-puts "Começaremos o jogo para você, " + nome
-puts "Escolhendo um número secreto entre 0 e 200..."
-numero_secreto = 175
-puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+def da_boas_vindas
+    puts "Bem vindo ao jogo da adivinhação"
+    puts "Qual é o seu nome?"
+    nome = gets
+    puts "\n\n\n\n\n\n"
+    puts "Começaremos o jogo para você, " + nome
+end
 
-limite_de_tentativas = 5
-for tentativa in 1..limite_de_tentativas
+def sorteia_numero_secreto
+    puts "Escolhendo um número secreto entre 0 e 200..."
+    sorteado = 175
+    puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+    sorteado
+end
+
+def pede_um_numero(tentativa, limite_de_tentativas)
     puts "\n\n\n\n"
     puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
     puts "Entre com o número"
     chute = gets
     puts "Será que acertou? Você chutou " + chute
+    chute.to_i
+end
 
-    acertou = numero_secreto == chute.to_i
-
+def verifica_se_acertou(numero_secreto, chute)
+    acertou = numero_secreto == chute
     if acertou
         puts "Acertou!"
-        break
+        return true
+    end
+    maior = numero_secreto > chute
+    if maior
+        puts "O número secreto é maior!"
     else
-        maior = numero_secreto > chute.to_i
-        if maior
-            puts "O número secreto é maior!"
-        else
-            puts "O número secreto é menor!"
-        end
+        puts "O número secreto é menor!"
+    end
+    false
+end
+
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
+
+limite_de_tentativas = 5
+for tentativa in 1..limite_de_tentativas
+    chute = pede_um_numero tentativa, limite_de_tentativas
+    if verifica_se_acertou numero_secreto, chute
+        break
     end
 end
